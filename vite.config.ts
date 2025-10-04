3c5e7e0e176d79f3f714e06089e05a8629d818b7
Zubeox <kingzubeox@gmail.com>
2025-10-04 21:33:33 +0300

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
@@ -8,5 +10,10 @@ export default defineConfig({
   },
   server: {
     port: 5173
+  },
+  resolve: {
+    alias: {
+      '@lib': path.resolve(__dirname, './src/lib'),
+    }
   }
 });
