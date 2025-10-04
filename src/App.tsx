65af0712be9e3e55a94bb29628ff15df1bd8810e
Zubeox <kingzubeox@gmail.com>
2025-10-04 12:06:12 +0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { supabase } from './lib/supabase';
+import { supabaseClient as supabase } from './lib/supabaseClient';
 import { LoginScreen } from './components/Auth/LoginScreen';
 import { Sidebar } from './components/Layout/Sidebar';
 import { Header } from './components/Layout/Header';
